e5a1400580a554a238ee3afcda8654478d98387a
devofficer <myeyesontech@gmail.com>
2022-10-30 06:37:52 -0700

    [init] initial structure with express + typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT ? process.env.PORT : 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express Typescript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Express TypeScript Server is running ${port}`);
+});
